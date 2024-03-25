b746ada9ff11563b9316be4bc360ae5a7a7a3ed4
alienbase <141658251+alienbasedev@users.noreply.github.com>
2024-03-25 19:35:24 +0100

    bump version

---
+++ dist/index.cjs
@@ -0,0 +1,7 @@
+/**
+     * @license
+     * tradingsoccerlib v0.1.1
+     * Copyright (c) 2019 beatercode.
+     * Licensed under Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
+     */
+"use strict";"production"===process.env.NODE_ENV?module.exports=require("./umd/tradingsoccerlib.min.js"):module.exports=require("./umd/tradingsoccerlib.js");
