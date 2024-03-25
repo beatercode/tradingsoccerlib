59e52f3f5a3c6f01dad3f0651288f8287b2819d6
alienbase <141658251+alienbasedev@users.noreply.github.com>
2024-03-25 19:50:53 +0100

    bump version

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,52 +1,54 @@
-import fs from 'fs'
-import chalk from 'chalk'
+import fs from "fs";
+import chalk from "chalk";
 
-import { resolvePath } from './utils.js'
+import { resolvePath } from "./utils.js";
 
-const buildDir = resolvePath('dist')
+const buildDir = resolvePath("dist");
 
-let totalFileCount = 0
-let deletedFileCount = 0
+let totalFileCount = 0;
+let deletedFileCount = 0;
 
-function eachFiles (dir) {
+function eachFiles(dir) {
   if (fs.existsSync(dir)) {
-    const files = fs.readdirSync(dir)
-    files.forEach(file => {
-      const path = resolvePath(file, dir)
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const path = resolvePath(file, dir);
       if (fs.statSync(path).isDirectory()) {
-        eachFiles(path)
+        eachFiles(path);
       } else {
-        totalFileCount++
+        totalFileCount++;
       }
-    })
+    });
   }
 }
 
-function deleteFiles (dir) {
+function deleteFiles(dir) {
   if (fs.existsSync(dir)) {
-    const files = fs.readdirSync(dir)
-    files.forEach(file => {
-      const path = resolvePath(file, dir)
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const path = resolvePath(file, dir);
       if (fs.statSync(path).isDirectory()) {
-        deleteFiles(path)
+        deleteFiles(path);
       } else {
-        fs.unlinkSync(path)
-        deletedFileCount++
-        process.stdout.clearLine(process.stdout);
-        process.stdout.cursorTo(0)
-        const percent = `${Math.round(deletedFileCount / totalFileCount * 100)}%`
-        process.stdout.write(`${chalk.blue(`${percent}(${deletedFileCount}/${totalFileCount}): ${file}`)}`, 'utf-8')
+        fs.unlinkSync(path);
+        deletedFileCount++;
+        // process.stdout.clearLine(process.stdout);
+        // process.stdout.cursorTo(0)
+        const percent = `${Math.round(
+          (deletedFileCount / totalFileCount) * 100
+        )}%`;
+        // process.stdout.write(`${chalk.blue(`${percent}(${deletedFileCount}/${totalFileCount}): ${file}`)}`, 'utf-8')
       }
-    })
-    fs.rmdirSync(dir)
+    });
+    fs.rmdirSync(dir);
   }
 }
 
-function clean () {
-  eachFiles(buildDir)
-  deleteFiles(buildDir)
+function clean() {
+  eachFiles(buildDir);
+  deleteFiles(buildDir);
 
-  console.log(chalk.green('\n\n✔️ Clean successfully.\n'))
+  console.log(chalk.green("\n\n✔️ Clean successfully.\n"));
 }
 
-clean()
+clean();
